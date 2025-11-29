4d59785315fc6dd1ec246036611dd4be2978dcf3
raniszewski-robert <raniszewskirobert27@gmail.com>
2025-11-29 23:58:48 +0100

    add textrank

--- textrank.py
+++ textrank.py
@@ -19,16 +19,6 @@ class TextRankSummarizer:
     oparty na podobieństwie zdań TF-IDF.
     """
 
-    def __init__(self, damping_factor=0.8):
-        """
-        Inicjalizuje sumaryzator.
-        Args:
-            damping_factor (float): Współczynnik tłumienia
-        """
-        self.damping_factor = damping_factor
-        self.stop_words = stopwords.words('english')
-        self.vectorizer = TfidfVectorizer()
-
     def _preprocess_text(self, text):
         """
         Prywatna metoda do wstępnego przetwarzania tekstu.
@@ -37,11 +27,13 @@ class TextRankSummarizer:
         sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!)\s', text)
         sentences = [s.strip() for s in sentences if s.strip()]
 
+        english_stop_words = stopwords.words('english')
+        
         cleaned_sentences = []
         for s in sentences:
             s_clean = re.sub(r'[^a-zA-Z\s]', '', s.lower())
             words = s_clean.split()
-            s_processed = ' '.join([word for word in words if word not in self.stop_words])
+            s_processed = ' '.join([word for word in words if word not in english_stop_words])
             cleaned_sentences.append(s_processed)
 
         return sentences, cleaned_sentences
@@ -57,39 +49,35 @@ class TextRankSummarizer:
         Returns:
             str: Wygenerowane streszczenie.
         """
-        original_sentences, cleaned_sentences = self._preprocess_text(input_text)
+        original_sentences, cleaned_sentences = self.preprocess_text(input_text)
 
         if not original_sentences or all(not s for s in cleaned_sentences):
             return ""
 
         num_sentences_to_select = max(1, int(len(original_sentences) * summary_percentage))
 
-        try:
-             # Używamy fit_transform na nowo dla każdego tekstu, aby stworzyć nowy słownik słów
-            sentence_vectors = self.vectorizer.fit_transform(cleaned_sentences)
-        except ValueError:
-            return ""
+        vectorizer = TfidfVectorizer()
+        
+        sentence_vectors = vectorizer.fit_transform(cleaned_sentences)
 
-        # Macierz Podobieństwa
-        # Obliczanie podobieństwa cosinusowego
         similarity_matrix = cosine_similarity(sentence_vectors)
-        
 
-        # TextRank
         graph = nx.from_numpy_array(similarity_matrix)
-        
-        # Obliczanie rankingu zdań
-        scores = nx.pagerank(graph) 
+
+        scores = nx.pagerank(graph)
         
         ranked_sentences = {original_sentences[i]: scores[i] for i in range(len(original_sentences))}
 
-        # N zdań o najwyższych wynikach
         best_sentences_with_score = nlargest(num_sentences_to_select, 
-                                             ranked_sentences.items(), 
-                                             key=lambda item: item[1])
-                                             
-        best_sentences_set = {sentence for sentence, _ in best_sentences_with_score}
+                                            ranked_sentences.items(), 
+                                            key=lambda item: item[1])
+                                            
+        best_sentences = [sentence for sentence, score in best_sentences_with_score]
+
+        final_summary = []
         
-        final_summary = [s for s in original_sentences if s in best_sentences_set]
+        for original_sentence in original_sentences:
+            if original_sentence in best_sentences:
+                final_summary.append(original_sentence)
                 
         return " ".join(final_summary)
