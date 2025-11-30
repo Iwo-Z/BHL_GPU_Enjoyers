59082abe3ce8358f096c7be8645a86667fd3a5a8
Albert <albert@albert.localdomain>
2025-11-30 04:17:50 +0100

    resolving

--- test.py
+++
@@ -1,9 +0,0 @@
-from model import Evaluation
-
-ev = Evaluation()
-print(ev.run_optimized_LLM("Hi"))
-
-# from textrank import TextRankSummarizer
-# tr = TextRankSummarizer()
-# summary = tr.summarize("Machine learning is a field of artificial intelligence that focuses on teaching. I am very simple super pretty ass gay :)", summary_percentage=0.9)
-# print(summary)
